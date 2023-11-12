850f860d7ee5d27160ddebefb6b5d8c01d7c89bd
Nick Vang <nick.vang279@gmail.com>
2023-11-12 13:08:57 -0800

    Revert "feng-initial-commit"
    This reverts commit ba9f8014718a9e2225d2801507201630ee19bfc1.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./components/Home";
-import Shop from "./components/Shop";
+import React from 'react'
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/shop">Shop</Link>
-          </li>
-        </ul>
-        <Routes>
-          <Route exact path="/" element={<Home />}></Route>
-          <Route exact path="/shop" element={<Shop />}></Route>
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div>
+      test
+    </div>
   );
 }
 
