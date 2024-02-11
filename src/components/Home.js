62307627dddf2f2e2760b33a2773c13b3260859b
Jonathan Shew <jonathanshew@csus.edu>
2024-02-10 17:24:53 -0800

    Adds homepage along with size adjustments depending on user device.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,62 @@
 import React from "react";
+import { Carousel, Container, Row, Col } from "react-bootstrap";
+import * as images from "./assets";
 
 function App() {
-  return <div>test</div>;
+  return (
+    <Container>
+      <Row>
+        <Carousel fade
+          interval={10000}
+          variant="dark"
+          prevLabel={null}
+          prevIcon={null}
+          //nextLabel={null}
+          //nextIcon={null}
+          style={{ paddingTop: '20px', paddingBottom: '20px' }}
+        >
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={images.carousel1}
+              alt={`Slide 1`}
+            />
+            <Carousel.Caption style={{ backgroundColor: 'rgba(255, 255, 255, 0.7)', padding: '10px' }}>
+              <h2>Locate Your Destination</h2>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={images.carousel2}
+              alt={`Slide 2`}
+            />
+            <Carousel.Caption style={{ backgroundColor: 'rgba(255, 255, 255, 0.7)', padding: '10px' }}>
+              <h2>Browse For Groceries</h2>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </Row>
+      <Row className="justify-content-center">
+        <Col xs={12} sm={6} md={3} className="text-center">
+          <h4><u>About Us</u></h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean laoreet neque viverra lacus ultricies, eu convallis dolor tristique. Aliquam nec tempor nunc. Pellentesque ut ipsum facilisis ex congue volutpat porttitor at massa. Maecenas eu malesuada nulla. Vestibulum erat nisl, finibus id lorem facilisis, dictum sodales sapien. Integer hendrerit commodo metus, congue elementum ipsum sodales sed. Quisque tincidunt commodo interdum.</p>
+        </Col>
+        <Col xs={12} sm={6} md={3} className="text-center">
+          <h4><u>What We Do</u></h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean laoreet neque viverra lacus ultricies, eu convallis dolor tristique. Aliquam nec tempor nunc. Pellentesque ut ipsum facilisis ex congue volutpat porttitor at massa. Maecenas eu malesuada nulla. Vestibulum erat nisl, finibus id lorem facilisis, dictum sodales sapien. Integer hendrerit commodo metus, congue elementum ipsum sodales sed. Quisque tincidunt commodo interdum.</p>
+        </Col>
+        <Col xs={12} sm={6} md={3} className="text-center">
+          <h4><u>Ordering For Your Stay</u></h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean laoreet neque viverra lacus ultricies, eu convallis dolor tristique. Aliquam nec tempor nunc. Pellentesque ut ipsum facilisis ex congue volutpat porttitor at massa. Maecenas eu malesuada nulla. Vestibulum erat nisl, finibus id lorem facilisis, dictum sodales sapien. Integer hendrerit commodo metus, congue elementum ipsum sodales sed. Quisque tincidunt commodo interdum.</p>
+        </Col>
+        <Col xs={12} sm={6} md={3} className="text-center">
+          <h4><u>Help Us Stock Your Stay</u></h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean laoreet neque viverra lacus ultricies, eu convallis dolor tristique. Aliquam nec tempor nunc. Pellentesque ut ipsum facilisis ex congue volutpat porttitor at massa. Maecenas eu malesuada nulla. Vestibulum erat nisl, finibus id lorem facilisis, dictum sodales sapien. Integer hendrerit commodo metus, congue elementum ipsum sodales sed. Quisque tincidunt commodo interdum.</p>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default App;
