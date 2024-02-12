d584204a98e42d891c726bfb8ef53934239d4a89
LuckyPhan13 <143470018+LuckyPhan13@users.noreply.github.com>
2024-02-11 18:48:52 -0800

    Revert "UI"

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import { React, useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Cart from "./components/Cart";
-import Footer from "./components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
-import LocationStay from "./components/LocationStay";
+import Footer from "./components/Footer";
 import Shop from "./components/Shop";
-import SubCategory from "./components/SubCategory";
+import LocationStay from "./components/LocationStay";
 import ViewAll from "./components/ViewAllPage/ViewAll";
-import Inventory from "./pages/employees/Inventory.jsx";
-import Layout from "./pages/employees/Layout.jsx";
-import Orders from "./pages/employees/Orders.jsx";
-import Rooms from "./pages/employees/Rooms.jsx";
-import Customers from "./pages/employees/Customers.jsx";
-import Analytics from "./pages/employees/Analytics.jsx";
+import SubCategory from "./components/SubCategory";
+import Cart from "./components/Cart";
+import Employee from "./components/Employee";
 
 const App = () => {
   const [cartQuantity, setCartQuantity] = useState("");
@@ -24,31 +19,20 @@ const App = () => {
 
   return (
     <BrowserRouter>
-      {window.location.pathname.split('/')[1] !== 'employee' &&
-        <Header getCartQuantity={cartQuantity} />
-      }
-      <Routes>
-        <Route exact path="/" element={<Home />}></Route>
-        <Route exact path="/shop" element={<Shop />}></Route>
-        <Route exact path="/location-stay" element={<LocationStay />}></Route>
-        <Route exact path="/view-all" element={<ViewAll />}></Route>
-        <Route exact path="/sub-category" element={<SubCategory />}></Route>
-        <Route
-          exact
-          path="/shopping-cart"
-          element={<Cart sendCartQuantity={getCartQuantity} />}
-        ></Route>
-
-        <Route path="/employee" element={<Layout />}>
-
-          <Route exact index element={<Orders />} />
-          <Route exact path="inventory" element={<Inventory />} />
-          <Route exact path="rooms" element={<Rooms />} />
-          <Route exact path="customers" element={<Customers />} />
-          <Route exact path="analytics" element={<Analytics />} />
-
-        </Route>
-      </Routes>
+      {window.location.pathname !== '/employee' && <Header getCartQuantity={cartQuantity} />}
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/shop" element={<Shop />}></Route>
+          <Route exact path="/location-stay" element={<LocationStay />}></Route>
+          <Route exact path="/view-all" element={<ViewAll />}></Route>
+          <Route exact path="/sub-category" element={<SubCategory />}></Route>
+          <Route
+            exact
+            path="/shopping-cart"
+            element={<Cart sendCartQuantity={getCartQuantity} />}
+          ></Route>
+          <Route exact path="/employee" element={<Employee />}></Route>
+        </Routes>
       <Footer />
     </BrowserRouter>
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import { Auth0Provider } from '@auth0/auth0-react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
