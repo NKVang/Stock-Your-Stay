b500b18e5bfb0cacfacf638badcb41f28e1f745a
Jonathan Shew <jonathanshew@csus.edu>
2024-03-10 18:01:11 -0700

    Cart.js Merge Issues fixes Fixed code duplication when merging NOC-125 Reverted NOC-121 merge conflict fix to use hardcoded images as NOC-121 used AirTable urls that had expired.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -10,18 +10,20 @@ import {
   Button,
 } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./shop_style.css";
 // import * as images from "./assets";
 
 // images hard coded so once passed to Stripe, it can display images at checkout
+// additionally cannot use urls passed from AirTable for long term testing as
+// those have an expiration attached
 const Cart = (props) => {
   const [cartItems, updatedItems] = useState([
     {
       id: 1,
       name: "Siggi's Mixed Berry Non Fat Yogurt 4 Pack",
-      image: "https://v5.airtableusercontent.com/v3/u/26/26/1710064800000/KJyXyL7iSGUrxnQo4OUgsQ/GRw_XhjwqVUADAKE5zTP1TN8ca0vnJHiJXKuo6OS1UEcuSOetFTAhQUTxnhN0fqWUsPLuyK_Xe_dEwgnLEr1Hz8dzsfKQInnSx6zcJqM5g6iOvaR9TsacWi8bY3TxT7sbTpExMt1AvfMK7t3I8x3eQ/_1X4PW4zWqERLQnBWymgFVkZn6YPwBrOjBEty1UENw8",
+      image:
+        "https://stockyourstay.minthouse.com/cdn/shop/products/81IxzTEA_2L._SL1500_1024x1024.jpg",
       quantity: 1,
       price: 5.59,
       pricePerQuantity: 5.59,
@@ -30,7 +32,8 @@ const Cart = (props) => {
     {
       id: 2,
       name: "Kerrygold Pure Irish Butter",
-      image: "https://v5.airtableusercontent.com/v3/u/26/26/1710064800000/_gX_-zQKxEuQGxmZ8gdz6A/WA2FMbYgIhbjwQQQTm5AqB7GTZxSQZXilNv7_3NZQC04S3Xel2WMxn1qB265lM-ZGhpxp8xN9P5JZK5CDimyfjIWmtnTqSEDZ6wun1xsyYWZf1121hNE2DJ4ycMOOmuvx4JvySJ4The5_XludZRh7A/cEJxlbYgJ-9hXMgBslIHxgTfdPHLozPU2IWrHoW6_gw",
+      image:
+        "https://stockyourstay.minthouse.com/cdn/shop/products/ScreenShot2021-09-01at3.10.03PM_1024x1024.png",
       quantity: 1,
       price: 5.59,
       pricePerQuantity: 5.59,
@@ -39,7 +42,8 @@ const Cart = (props) => {
     {
       id: 3,
       name: "Organic Mozzarella Sticks",
-      image: "https://v5.airtableusercontent.com/v3/u/26/26/1710064800000/1OYBVcih1qW68Lv7Dpt-OQ/Ndq8pxhWi7zi-1-PGAa3nFTVWuZO9AoTpwt_acnmYZAqodyRVcM3eSPG-JpZT36D-I-Evrnz8RKMRsM-VsgUhxz84UGSHMEZb48z4Rd9tYsYmpolzqNW8OyJJjA9uZkmcHmfiepeZ8p5apXuD4-FlA/1h_kt0ADfnMRfOF7eehuM_b6pwJhL_rfGZeXJiL-adI",
+      image:
+        "https://stockyourstay.minthouse.com/cdn/shop/products/ScreenShot2021-10-06at3.01.45PM_1024x1024.png",
       quantity: 1,
       price: 5.59,
       pricePerQuantity: 5.59,
@@ -48,7 +52,8 @@ const Cart = (props) => {
     {
       id: 4,
       name: "Organic Nonfat Milk, 1 Quart",
-      image: "https://v5.airtableusercontent.com/v3/u/26/26/1710064800000/cz1jRAChiKzrpqEm33TQiw/mJPayT43wwCwIW1ESiYfq0bW0k71KGZ3doU1S7JlS0ZT8xOn7bbymqjo5yns8pYH6J18RkK6A7pRbnFZ-SKDUcikP0tkTt1L6hz1ZPJd44-agATU_GsIKppLfQpfmQOZH9W8Vbv6FdJDOheEVDBoFA/hy5kB4KbnwZbA6dKPXDo8MLVgf8GboKM5U3i2WqzooA",
+      image:
+        "https://stockyourstay.minthouse.com/cdn/shop/products/ScreenShot2021-09-01at5.10.16PM_600x600.png",
       quantity: 1,
       price: 5.59,
       pricePerQuantity: 5.59,
@@ -57,8 +62,9 @@ const Cart = (props) => {
     {
       id: 5,
       name: "Chicken & Apple Chicken Sausage",
-      image: "https://v5.airtableusercontent.com/v3/u/26/26/1710064800000/0niphafBWr7O4IZOhTQWEw/NtXyETCxYruWlNJbTuQDJIpYbRNRCxR1j0xBLwPCvO7atkm4D4ZbWV4eIc5OsjH0_Y9jPJkhQ3OKbGhAkHfjy48_oGpt2QLa9eCtTKIEnvojnuJHRox509-LYREFUYbd3ZawDD0ZlrRZvkmP4Fs0KQ/IMiCeLkAVmN5WhpDaTQPlmAvrmULucRzf3nfd7NZjNI",
-      quantity: 1,
+      image:
+        "https://stockyourstay.minthouse.com/cdn/shop/products/ScreenShot2021-11-11at10.56.26AM_1024x1024.png",
+        quantity: 1,
       price: 9.29,
       pricePerQuantity: 9.29,
       rId: "recVeoABJ2kjHotjo",
@@ -66,7 +72,8 @@ const Cart = (props) => {
     {
       id: 6,
       name: "RAOS Chicken Parmesan",
-      image: "https://v5.airtableusercontent.com/v3/u/26/26/1710064800000/5vcGdFJ4k0l9NLDWvTq7Qg/W1AFpHI_IfCB2wdsi76Zrph5jZrDa9uoKunaKb-xH5QLZzu7fP_AbB-nt0jgsruZ1xRIbFEOFgqCYsNA6KB2uApF3ogLttJLp1P3lkae7Z_QB8WsidTz2zDNrBfrqtsH7fNyLuRrF3Zhs4QYvcd1Ww/znlSbCXWCVxGSDc0mAMwIFde9tKHkBohecRURWeMDAs",
+      image:
+        "https://stockyourstay.minthouse.com/cdn/shop/products/81OjUj755IL._SL1500_1200x1200.jpg",
       quantity: 1,
       price: 6.99,
       pricePerQuantity: 6.99,
@@ -75,7 +82,8 @@ const Cart = (props) => {
     {
       id: 7,
       name: "Applegate Chicken Nuggets",
-      image: "https://v5.airtableusercontent.com/v3/u/26/26/1710064800000/MfuRI4nkhry_C2Y_lIIJcA/PtoVcvZ7IhFt5CH5A7z6a9Wrz4j5ldlVDNlzONbsXPJ60FWQwu22VvTPEOIPLapSBIUzWelwSH07xohlekd5r9ufVzeqCPNfxV2crpDuGszXVQKNMA02jWmWQVaFsCuquhmeLucoM88-a0dioY-edg/nlgmr8DUCzkW_GTvsL6LJRIvyO0iXi882VmfDsyI4OA",
+      image:
+        "https://stockyourstay.minthouse.com/cdn/shop/products/ScreenShot2021-10-06at3.00.57PM_1024x1024.png",
       quantity: 1,
       price: 12.99,
       pricePerQuantity: 12.99,
@@ -84,7 +92,8 @@ const Cart = (props) => {
     {
       id: 8,
       name: "Frozen Salmon Fillets (2), 6oz/each",
-      image: "https://v5.airtableusercontent.com/v3/u/26/26/1710064800000/p6aAhprUArb8E-f6eX1iOQ/sgygAOUKKnPu6Yc5efK79kbuHWEFfDAjts8dqH3yzrnKqLAW3BecMC-QgSK58UAgqLjRXXKcEjV1X7RnhPmRYY_v5-2qshU3N465yGqjA-m_NR4hNcbz4RpxGf-EeIWqU9ovbGl5ZYUIrGKBtF2fVw/vGaK0T9FBs2wPjjSYtizPCht_jspSIPCxTQrPJN0OTQ",
+      image:
+        "https://stockyourstay.minthouse.com/cdn/shop/products/81gK3bRQuhL._SL1500_1024x1024.jpg",
       quantity: 1,
       price: 13.99,
       pricePerQuantity: 13.99,
@@ -269,59 +278,6 @@ const Cart = (props) => {
           </Col>
 
         {/* Right column */}
-        <Col xs={12} md={6} className="shop-container">
-          <Row className="cart-total">
-            <Container fluid className="right-cart-container">
-              <div className="cart-total-main-container">
-                <Row>
-                  <Col>
-                    <Row>Delivery Date: 12-31-23</Row>
-                    <Row>Location: Austin, TX</Row>
-                    <Row style={{ marginTop: 50 }}>
-                      Subtotal (
-                      {cartQuantity > 1 || cartQuantity === 0
-                        ? cartQuantity + " items"
-                        : cartQuantity + " item"}
-                      ):
-                      <b style={{ paddingLeft: 0 }}>${totalPrice}</b>
-                    </Row>
-                    <Row>
-                      <i className="small-text" style={{ paddingLeft: 0 }}>
-                        Taxes and shipping calculated at checkout
-                      </i>
-                    </Row>
-                  </Col>
-                  <Row>
-                    <h6 style={{ paddingLeft: 0, marginTop: 30 }}>
-                      <i>Special Instructions For Seller:</i>
-                    </h6>
-                    <Form>
-                      <Form.Group>
-                        <Form.Label></Form.Label>
-                        <Form.Control as="textarea" rows={5} />
-                      </Form.Group>
-                    </Form>
-                  </Row>
-                  <Row style={{ marginTop: 10 }}>
-                    <Col xs="auto">
-                      <Button variant="dark">Continue Shopping</Button>
-                      <Button variant="success" onClick={checkOut}>
-                        Check Out
-                      </Button>
-                      <br />
-                      **
-                      <strong>
-                        Note: Only orders placed within 48 hours of check-in
-                        will be fufilled. **
-                      </strong>
-                    </Col>
-                  </Row>
-                </Row>
-              </div>
-            </Container>
-          </Row>
-        </Col>
-      </Row>
           <Col xs={12} md={6} className="shop-container">
             <Row className="cart-total">
               <Container fluid className="right-cart-container">
