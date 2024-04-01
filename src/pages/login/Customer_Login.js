ad49ea32aece30bcfd875bd68d17dbc8c6acdc61
Nick Vang <nick.vang279@gmail.com>
2024-03-31 21:55:29 -0700

    #NOC-126
    When the customer enters data into the login text fields, the username and password inputs are checked and validated. If the login is succesful, then it navigate to the reservation page, if not, then ask to try again.

--- src/pages/login/Customer_Login.js
+++ src/pages/login/Customer_Login.js
@@ -2,9 +2,45 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { Button, Card, Container, Form, Stack } from 'react-bootstrap';
 import './App.css';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
+
+async function mockAuthenticate(username, password) {
+  if (username === 'user' && password === 'pass') {
+    return 'fakeToken1A';
+  }
+  throw new Error('Authentication failed');
+}
 
 function App() {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const usernameInput = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const passwordInput = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+      try {
+        const token = await mockAuthenticate(username, password);
+        console.log('Login successful');
+        alert('Login Successful. Redirecting to Shop.');
+        localStorage.setItem('token', token);
+        navigate('/reservations');
+      } catch (error) {
+        console.error('Login failed');
+        alert('Invalid Login. Please Try Again.');
+      }
+  };
+
   return (
     <div className='App'>
       <header className="App-header">
@@ -17,18 +53,16 @@ function App() {
                     <h1> Login </h1>
 
                     <Form.Group>
-                      <h6> Last Name </h6>
-                      <Form.Control type="text" placeholder="Last name here" />
+                      <h6> Username </h6>
+                      <Form.Control type="text" value= {username} onChange = {usernameInput} placeholder="username" />
                     </Form.Group>
 
                     <Form.Group>
-                      <h6> Confirmation Number</h6>
-                      <Form.Control type="number" placeholder="xx-xxx-xx" />
+                      <h6> Password</h6>
+                      <Form.Control type="text" value= {password} onChange = {passwordInput} placeholder="password" />
                     </Form.Group>
-                    {/*has issue in which user is able to increment/decrement using buttons 
-                that appear when entering number */}
 
-                    <Button variant="primary">Shop Now</Button>
+                    <Button variant="primary" onClick = {handleLogin}>Login</Button>
 
                   </Stack>
                 </Form>
