f05ddf536d7e212ae97e1ab4a09007c11265ec3e
Nick Vang <nick.vang279@gmail.com>
2024-05-02 17:05:30 -0700

    Removed All Unecessary Files

--- src/components/assets/index.js
+++ src/components/assets/index.js
@@ -1,51 +1,15 @@
-import img1 from "./1.webp";
-import img2 from "./2.webp";
-import img3 from "./3.webp";
-import img4 from "./4.webp";
-import img5 from "./5.webp";
-import img6 from "./6.webp";
-import img7 from "./7.webp";
-import img8 from "./8.webp";
-import img9 from "./9.webp";
-import img10 from "./10.webp";
-import img11 from "./11.webp";
-import img12 from "./12.webp";
-import carousel1 from "./carousel1.webp";
-import carousel2 from "./carousel2.webp";
-import addtocart from "./addtocart.png";
-import quantitybutton from "./quantitybutton.png";
 import visa from "./visa.png";
 import mastercard from "./mastercard.png";
 import amex from "./amex.png";
 import discover from "./discover.png";
 import diners from "./diners.png";
 import jcb from "./jcb.png";
-import splash1 from "./splash1.jpeg";
-import splash2 from "./splash2.webp";
 
 export {
-  img1,
-  img2,
-  img3,
-  img4,
-  img5,
-  img6,
-  img7,
-  img8,
-  img9,
-  img10,
-  img11,
-  img12,
-  carousel1,
-  carousel2,
-  addtocart,
-  quantitybutton,
   visa,
   mastercard,
   amex,
   discover,
   diners,
   jcb,
-  splash1,
-  splash2,
 };
